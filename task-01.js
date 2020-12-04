f579c0156a672ab32506df9bc261da7f4ecd031d
Andrii Dorohov <70909934+AndriiDorohov@users.noreply.github.com>
2020-12-04 22:19:19 +0200

    Delete task-01.js

--- task-01.js
+++
@@ -1,8 +0,0 @@
-const logItems = function(array) {
-    for (let i = 0; array.length > i; i++)
-    console.log(`Номер элемента: ${i+1}, значение элемента: ${array[i]}`)
-    return  
-};
-
-logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
-logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
